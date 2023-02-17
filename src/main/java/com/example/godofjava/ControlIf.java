07ec2d7c4de88a8738f7e357780406cef5861261
김한선 <68306239+hansun-hub@users.noreply.github.com>
2023-02-17 19:42:43 +0900

    2023-02-17

--- src/main/java/com/example/godofjava/ControlIf.java
+++ src/main/java/com/example/godofjava/ControlIf.java
@@ -4,6 +4,8 @@ public class ControlIf {
     public static void main(String args[]){
 
         ControlIf control = new ControlIf();
+//        control.ifStatement();
+        control.ifElseStatement();
     }
 
     public void ifStatement(){
@@ -18,6 +20,29 @@ public class ControlIf {
         int intValue=10;
 
         if(intValue > 5) System.out.println("It's bigger than 5");
-        else System.out.println("I");
+        else System.out.println("It's smaller or equal than 5.");
+
+        if(intValue <= 5)
+            System.out.println("It's smaller or equal than 5.");
+        else System.out.println("It's bigger than 5");
+    }
+
+    public class ControlIfAndOr{
+        public void main(String args[]){
+            ControlIfAndOr control = new ControlIfAndOr();
+            control.ifAndOr();
+        }
+
+        public void ifAndOr(){
+            int age = 25;
+            boolean isMarried = true;
+
+            if(age>25 && isMarried){
+                System.out.println("Age is over 25 and Married");
+            }
+            if(age>25 || isMarried){
+                System.out.println("Age is over 25 or Married");
+            }
+        }
     }
 }
